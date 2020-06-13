d0d951998fb7df44b88945b4d5a9321a6dc0bb8c
Camilo Alvarez <camiloalvarez0521@gmail.com>
2020-06-12 22:13:50 -0500

    Servicios CRUD Usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const _ = require("underscore");
 const Usuario = require("../models/usuario");
 const app = express();
 
@@ -28,3 +29,75 @@ app.post("/usuario", (req, res) => {
 });
 
 module.exports = app;
+
+//obtener usuarios
+app.get("/usuario", (req, res) => {
+  Usuario.find({ estado: true }, "nombre email rol").exec((err, usuarios) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    res.json({
+      ok: true,
+      usuarios,
+    });
+  });
+});
+
+//actualizar usuarios
+
+app.put("/usuario/:id", (req, res) => {
+  let id = req.params.id;
+  let body = req.body;
+  if (body.password) {
+    body.password = bcrypt.hashSync(body.password, 10);
+  }
+
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true },
+    (err, usuarioDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+      res.json({
+        ok: true,
+        usuario: usuarioDB,
+      });
+    }
+  );
+});
+
+//eliminar usuario
+app.delete("/usuario/:id", (req, res) => {
+  let id = req.params.id;
+  let borrar = {
+    estado: false,
+  };
+  Usuario.findByIdAndUpdate(id, borrar, (err, borrado) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    if (!borrado) {
+      res.status(400).json({
+        ok: false,
+        err: {
+          message: "Usuario no encontrado",
+        },
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: borrado,
+    });
+  });
+});
